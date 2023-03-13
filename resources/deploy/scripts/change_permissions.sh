82a330470a1d12aeae41e5e6028723f98865b160
Pann Ei Khaing <scm.panneikhaing@gmail.com>
2023-03-13 09:35:30 +0630

    [main] fix appspec.yml file

---
+++ resources/deploy/scripts/change_permissions.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo -i -u ec2-user composer install --working-dir=/var/www/html/example_deploy_laravel_app
---
+++ resources/deploy/scripts/delete_old.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# rm -rf /var/www/html/hello_AWS_with_laravel
+echo 'hello'
